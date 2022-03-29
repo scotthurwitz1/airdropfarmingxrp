4977a0a1c0a329a6f81cd8649b8eed160dcba9ee
Scott Hurwitz <74121226+scotthurwitz1@users.noreply.github.com>
2022-03-29 17:10:42 -0400

    Add files via upload

---
+++ account data export.py
@@ -0,0 +1,28 @@
+#creates txt document with seeds
+
+import xrpl
+import pandas as pd
+from xrpl.wallet import Wallet
+seeds = []
+addresses = []
+num = 2650
+filename = 'redacted'
+
+######fix filenames before running
+
+for i in range(num):
+    a = xrpl.core.keypairs.generate_seed()
+    seeds.append(a)
+
+for i in seeds:
+    address = Wallet(seed=i, sequence=12).classic_address
+    addresses.append(address)
+
+df = pd.DataFrame()
+df['Seeds']=seeds
+df['Addresses']=addresses
+print(df)
+
+df.to_excel(f'/Users/napoleon/Desktop/MyCode/{filename}.xlsx', index = False)
+
+
